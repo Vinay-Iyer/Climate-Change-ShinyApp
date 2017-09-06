5647b00c92005244335aa8af5c4b279a8ac13464
Vinay-Iyer <venkatramani.r@gmail.com>
2017-09-06 19:18:48 +0200

    data reprocessed

--- scripts/testplots.R
+++ scripts/testplots.R
@@ -1,6 +1,6 @@
 library(plotly)
 setwd('/Git Projects/Climate-Change-ShinyApp')
-
+rm(list=ls())
 # Import libraries 
 library(shiny)
 library(plotly)
@@ -9,24 +9,28 @@ library(ggthemes) # themes to assist ggplot
 library(data.table) # for fast reading and data manipulation
 library(tidyr)
 
+# Data preprocessing
+# ====================================================================================
 data <- read.csv('Data/GlobalLandTemperaturesByState.csv', TRUE,",")
 
 head(data)
+data <- na.omit(data)
 
-row.has.na <- apply(data, 1, function(x){any(is.na(x))})
-sum(row.has.na)
-data <- data[!row.has.na,]
 data <- separate(data,col = dt, into = c("Year", "Month", "Day"), convert = TRUE)
-data<- filter(data,Year>1930)
-
+#data_new <- summarise(data_new,Temp = mean(AverageTemperature))
 data_new <- filter(data,Country=="India")
+vars <- c(data_new$Year,data_new$AverageTemperature)
 
+data_new %>% 
+  filter(Year>1900) %>%
+  group_by(Year) %>% 
+  summarise(Temp = mean(AverageTemperature)) ->data1
 
+# ====================================================================================
+# Plot area. 
 
-
-
-
-
-
+png(filename="plots/plot1.png")
+ggplot(data1, aes(x=Year, y=Temp)) + geom_path(colour = "grey50") + geom_point(aes(colour=Year))
+dev.off()
 
 
