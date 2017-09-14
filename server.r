7021d93fe34de0dab8d77dbc3b50183ca6c1e604
Vinay-Iyer <venkatramani.r@gmail.com>
2017-09-14 20:18:00 +0200

    fixed server.r errors

--- server.r
+++ server.r
@@ -1,8 +1,5 @@
 rm(list=ls())
-<<<<<<< HEAD
 setwd('/Git Projects/Climate-Change-ShinyApp')
-=======
->>>>>>> 3a4bfee812f4028a82e549ba30a41b1e57211a4f
 
 # Import libraries 
 library(shiny)
@@ -10,12 +7,8 @@ library(plotly)
 library(ggplot2)
 library(ggthemes) # themes to assist ggplot
 library(data.table) # for fast reading and data manipulation
-<<<<<<< HEAD
 library(tidyr)
-=======
 library('tidyr')
->>>>>>> 3a4bfee812f4028a82e549ba30a41b1e57211a4f
-
 
 data <- read.csv('Data/GlobalLandTemperaturesByState.csv', TRUE,",")
 head(data)
@@ -24,12 +17,7 @@ row.has.na <- apply(data, 1, function(x){any(is.na(x))})
 sum(row.has.na)
 data <- data[!row.has.na,]
 data <- separate(data,col = dt, into = c("Year", "Month", "Day"), convert = TRUE)
-<<<<<<< HEAD
 data<- filter(data,Year>1930)
-=======
-
->>>>>>> 3a4bfee812f4028a82e549ba30a41b1e57211a4f
-
 # =============================================================================== 
 
 shinyServer(
@@ -37,8 +25,7 @@ shinyServer(
     
     # testing a simple plot 
     output$myplot <- renderPlot( {
-<<<<<<< HEAD
-      
+
       data_new <- filter(data,Country==input$plot_ctry)
       #data_new <- group_by(Year)
       #data_new <- summarise(data_new,Temp = mean(AverageTemperature))
@@ -47,8 +34,7 @@ shinyServer(
         group_by(Year) %>% 
         summarise(Temp = mean(AverageTemperature)) -> data_new1
       
-=======
-    
+
     data_new <- filter(data,Country==input$plot_ctry)
     #data_new <- filter(data_new,Year>1900)
     #data_new <- group_by(Year)
@@ -59,7 +45,6 @@ shinyServer(
       group_by(Year) %>% 
       summarise(Temp = mean(AverageTemperature)) ->data_new1
     
->>>>>>> 3a4bfee812f4028a82e549ba30a41b1e57211a4f
       qplot(Year,Temp, data=data_new1, main="Average Temperature 1930-2013",
             geom=c("line","jitter","smooth"))+ aes(colour = Temp) + 
         scale_color_gradient(low="yellow", high="red")
@@ -72,8 +57,3 @@ shinyServer(
 
 # ===============================================================================
 
-<<<<<<< HEAD
-=======
-
-
->>>>>>> 3a4bfee812f4028a82e549ba30a41b1e57211a4f
--- ui.r
+++ ui.r
@@ -1,11 +1,6 @@
 library(shiny)
 
 shinyUI(fluidPage(
-<<<<<<< HEAD
-  
-=======
-
->>>>>>> 3a4bfee812f4028a82e549ba30a41b1e57211a4f
   titlePanel(title = h2("Climate change",align="center")),
   sidebarLayout(position = "right",
                 # Sidebar panel            
@@ -14,31 +9,19 @@ shinyUI(fluidPage(
                              fluidRow(
                                column(width = 4, wellPanel(
                                  radioButtons("plot_ctry", "Select country",
-<<<<<<< HEAD
                                               c("India", "United States", "Canada"))
                                  
                                )
-                               )
-=======
-                                              c("India", "United States"))
-                                 
+
                                  )
                               )
->>>>>>> 3a4bfee812f4028a82e549ba30a41b1e57211a4f
                              ),
                              br()
                 ),
                 
-<<<<<<< HEAD
-                
-                # Main Panel
-                mainPanel(tabPanel("Plots", plotOutput("myplot")))
-=======
-  
-  # Main Panel
-  mainPanel(tabPanel("Plots", plotOutput("myplot")))
->>>>>>> 3a4bfee812f4028a82e549ba30a41b1e57211a4f
+
+          # Main Panel
+         mainPanel(tabPanel("Plots", plotOutput("myplot")))
   )
   
-)
 )
